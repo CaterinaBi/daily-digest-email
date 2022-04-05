4b7aeea6f0707df5712693668d1a36b455f2785b
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-04-05 21:48:43 +0100

    Creates initial functions

--- content.py
+++ content.py
@@ -1,1 +1,14 @@
+def get_random_quote():
+    pass
 
+def get_weather_forecast():
+    pass
+
+def get_youtube_shorts():
+    pass
+
+def get_wikipedia_article():
+    pass
+
+if __name__ == '__main__':
+    pass #test code
