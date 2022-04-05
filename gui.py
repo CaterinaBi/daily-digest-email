2d3f8a3b8ad576adba0108a9723918f9f6eba5df
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-04-05 21:56:37 +0100

    Creates DailyDigestGUI class

--- gui.py
+++ gui.py
@@ -1,1 +1,20 @@
+from tkinter import *
+from tkinter import tkk
 
+class DailyDigestGUI:
+
+    def __init__(self, root)
+        pass #build the DailyDigestGUI
+
+    """
+    The DailyDigestGUI should enable the admin to...
+    - configure the content sources to be included in the email
+    - add/remove recepients
+    - schedule daily time to send the email
+    - configure sender credentials
+    """
+
+if __name__ == '__main__':
+    root = Tk()
+    app = DailyDigestGUI(root)
+    root.mainloop()
